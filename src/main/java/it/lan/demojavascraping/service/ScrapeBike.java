b9940ecc3033474ef26c8c236380d56ab0a2437c
Marco Lanfranchi <>
2021-03-24 22:18:27 +0100

    Implementazione email basic

--- src/main/java/it/lan/demojavascraping/service/ScrapeBike.java
+++ src/main/java/it/lan/demojavascraping/service/ScrapeBike.java
@@ -1,5 +1,6 @@
 package it.lan.demojavascraping.service;
 
+import it.lan.demojavascraping.controller.FeedbackController;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -23,8 +24,14 @@ public class ScrapeBike {
     private final Logger logger = LoggerFactory.getLogger(ScrapeBike.class);
     private static String url = "https://www.decathlon.it/p/mtb-st-540-grigio-rosso-27-5/_/R-p-301097?mc=8529702&c=BEIGE";
 
+    private final FeedbackController feedbackController;
+
+    public ScrapeBike(FeedbackController feedbackController) {
+        this.feedbackController = feedbackController;
+    }
+
     @Async
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(fixedRate = 90000)
     public void getAvailability() {
         Document webPage;
 
@@ -33,10 +40,13 @@ public class ScrapeBike {
                     .connect(url)
                     .get();
 
+            StringBuilder res = new StringBuilder();
+
             Element title = webPage.getElementsByClass("title--main product-title-right").get(0);
             Element color = webPage.getElementsByClass("model-title").get(0);
             logger.info("{}", title.text());
             logger.info("{}", color.text());
+            res.append(title.text()).append(" ").append(color.text()).append(" ");
 
             Elements ulChildrens = webPage.getElementsByClass("sizes__list").get(0).children();
             //logger.info("{}", ulChildrens.size());
@@ -51,11 +61,16 @@ public class ScrapeBike {
                 logger.info("{} {}", size, disponibilita);
 
                 codeValue = ilChild.dataset();
+
+                res.append(size).append(" ").append(disponibilita).append(" ");
             }
+            feedbackController.sendInfo(res.toString());
 
         } catch (IOException e) {
             logger.info("Can't connect to: {}", url);
             e.printStackTrace();
         }
+
+
     }
 }
--- src/main/java/it/lan/demojavascraping/task/ScheduledTask.java
+++ src/main/java/it/lan/demojavascraping/task/ScheduledTask.java
@@ -1,13 +1,7 @@
 package it.lan.demojavascraping.task;
 
-import it.lan.demojavascraping.service.ScrapeData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import it.lan.demojavascraping.service.ScrapeBike;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
 
 public class ScheduledTask {
     private final Logger logger = LoggerFactory.getLogger(ScheduledTask.class);
